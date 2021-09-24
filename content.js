1518ce943f711b434e129056dab832d7ef362246
Melike Doğru <dogrumelike98@gmail.com>
2021-09-24 14:18:30 +0300

    All inputs can be saved
    All input types can be obtained and saved. Submit button function added.

--- content.js
+++ content.js
@@ -28,15 +28,14 @@ const input = document.getElementsByTagName('input');
 console.log(input)
 
 const phone = document.getElementsByClassName("mask-phone-number form-textbox validate[Fill Mask]");
-
 const date = document.getElementsByClassName("form-textbox validate[limitDate, validateLiteDate]");
+const dateRequired = document.getElementsByClassName("form-textbox validate[required, limitDate, validateLiteDate]");
 const dateIcon = document.getElementsByClassName(" newDefaultTheme-dateIcon icon-liteMode");
 const dateCalendarpopup = document.getElementsByClassName("calendar popup");
 const datePreviousMonthButton = document.getElementsByClassName("calendar-new-header");
 const dateNextMonthButton = document.getElementsByClassName("button nextMonth");
 const datePreviousYearButton = document.getElementsByClassName("button previousYear");
 const dateNextYearButton = document.getElementsByClassName("button nextYear");
-
 const product = document.getElementsByClassName("form-checkbox form-product-input")
 const selects = document.getElementsByTagName('select');
 const textareas = document.getElementsByTagName('textarea');
@@ -46,17 +45,10 @@ const time = document.getElementsByClassName("time-dropdown form-textbox")
 const spinnerInput = document.getElementsByClassName("form-spinner-input  form-textbox")
 const spinnerUpButton = document.getElementsByClassName("form-spinner-button form-spinner-up")
 const spinnerDownButton = document.getElementsByClassName("form-spinner-button form-spinner-down")
-console.log(spinnerInput)
-
 const radioButton = document.getElementsByClassName("form-radio")
-
-
-for (var i = 0; i < input.length; i++) {
-    console.log("hh")
-    if (input[i].className == "form-checkbox  form-product-input") {
-        console.log("forrrmm")
-    }
-}
+const starRating = document.getElementsByClassName("form-star-rating-star Stars")
+const submitButton = document.getElementsByClassName("form-submit-button submit-button jf-form-buttons jsTest-submitField")
+        
 
 var archive = [],
     keys = Object.keys(localStorage),
@@ -67,6 +59,7 @@ for (; key = keys[i]; i++) {
 }
 console.log(archive)
 
+//Get values for inputs from local storage
 for (var i = 0; i < input.length; i++) {
     for (var j = 0; j < archive.length; j++) {
         if (input[i].id == archive[j].key) {
@@ -75,12 +68,10 @@ for (var i = 0; i < input.length; i++) {
             for (var k = 0; k < product.length; k++) {
                 if (input[i].id === product[k].id) {
                     if (archive[j].value == "false") {
-                        //console.log("false")
                         product[k].checked = false
                         element.checked = false
                     }
                     if (archive[j].value == "true") {
-                        //console.log("true")
                         product[k].checked = true
                         element.checked = true
                     }
@@ -89,12 +80,10 @@ for (var i = 0; i < input.length; i++) {
             for (var k = 0; k < checkboxs.length; k++) {
                 if (input[i].id === checkboxs[k].id) {
                     if (archive[j].value == "false") {
-                        //console.log("false")
                         checkboxs[k].checked = false
                         element.checked = false
                     }
                     if (archive[j].value == "true") {
-                        //console.log("true")
                         checkboxs[k].checked = true
                         element.checked = true
                     }
@@ -103,12 +92,10 @@ for (var i = 0; i < input.length; i++) {
             for (var k = 0; k < radioButton.length; k++) {
                 if (input[i].id === radioButton[k].id) {
                     if (archive[j].value == "false") {
-                        //console.log("false")
                         radioButton[k].checked = false
                         element.checked = false
                     }
                     if (archive[j].value == "true") {
-                        //console.log("true")
                         radioButton[k].checked = true
                         element.checked = true
                     }
@@ -118,6 +105,7 @@ for (var i = 0; i < input.length; i++) {
     }
 }
 
+//Get values for selects from local storage
 for (var i = 0; i < selects.length; i++) {
     for (var j = 0; j < archive.length; j++) {
         if (selects[i].id == archive[j].key) {
@@ -127,6 +115,7 @@ for (var i = 0; i < selects.length; i++) {
     }
 }
 
+//Get values for textareas from local storage
 for (var i = 0; i < textareas.length; i++) {
     for (var j = 0; j < archive.length; j++) {
         if (textareas[i].id == archive[j].key) {
@@ -136,20 +125,42 @@ for (var i = 0; i < textareas.length; i++) {
     }
 }
 
+//Get values for star ratings from local storage
+for (var i = 0; i < starRating.length; i++) {
+    for (var j = 0; j < archive.length; j++) {
+        if (starRating[i].ariaLabel == archive[j].key) {
+            console.log(archive[j].key)
+            console.log(archive[j].value)
+            starRating[i].ariaChecked = archive[j].value
+            if (archive[j].value == "true"){
+                var countx = i;
+                console.log("hhh")
+                for (var l = 0; l <= countx; l++){
+                    starRating[l].className = "form-star-rating-star Stars rated"
+                }
 
+            }
+        }
+    }
+}
+
+//Date
 for (var j = 0; j < dateIcon.length; j++) {
     dateIcon[j].onclick = function () {
         console.log("icon")
         getSetDate()
+        getSetDateRequired()
     }
 }
 
 for (var j = 0; j < dateCalendarpopup.length; j++) {
     dateCalendarpopup[j].onclick = function () {
         getSetDate()
+        getSetDateRequired()
     }
 }
 
+//Product
 for (var j = 0; j < product.length; j++) {
     product[j].onclick = function () {
         console.log("click")
@@ -167,6 +178,7 @@ for (var j = 0; j < product.length; j++) {
     });
 }
 
+//Checkboxs
 for (var j = 0; j < checkboxs.length; j++) {
     checkboxs[j].onclick = function () {
         console.log("click")
@@ -175,15 +187,15 @@ for (var j = 0; j < checkboxs.length; j++) {
     }
 }
 
-
 for (var j = 0; j < checkboxs.length; j++) {
     checkboxs[j].addEventListener = ('product', function () {
         //console.log("listener")
-        console.log(this.checked)
+        //console.log(this.checked)
         localStorage.setItem(this.id, this.checked);
     });
 }
 
+//Spinners
 for (var j = 0; j < spinnerUpButton.length; j++) {
     spinnerUpButton[j].onclick = function () {
         console.log("click")
@@ -203,29 +215,27 @@ for (var j = 0; j < spinnerDownButton.length; j++) {
         }
     }
 }
-/*
+
+//Time
 for (var j = 0; j < time.length; j++) {
     time[j].onclick = function () {
         console.log("click")
         console.log(this.value)
         localStorage.setItem(this.id, this.value);
     }
-}*/
-
+}
 
 for (var j = 0; j < time.length; j++) {
     time[j].addEventListener = ('product', function () {
-        //console.log("listener")
-        console.log(this.value)
         localStorage.setItem(this.id, this.value);
     });
 }
 
+//Radio Buttons
 for (var j = 0; j < radioButton.length; j++) {
     radioButton[j].onclick = function () {
         console.log("radiobutton click")
         console.log(this.checked)
-        //localStorage.setItem(this.id, this.checked);
         for (var k = 0; k < radioButton.length; k++) {
             console.log(radioButton[k].checked)
             localStorage.setItem(radioButton[k].id, radioButton[k].checked);
@@ -233,29 +243,44 @@ for (var j = 0; j < radioButton.length; j++) {
     }
 }
 
-
 for (var j = 0; j < radioButton.length; j++) {
     radioButton[j].addEventListener = ('product', function () {
-        //console.log("radio button listener")
-        //console.log(this.checked)
         localStorage.setItem(this.id, this.checked);
     });
 }
 
+//Star Ratings
+for (var j = 0; j < starRating.length; j++) {
+    starRating[j].onclick = function () {
+        console.log("star click")
+        console.log(this.ariaChecked)
+        for (var k = 0; k < starRating.length; k++){
+            console.log(starRating[k].ariaChecked)
+            localStorage.setItem(starRating[k].ariaLabel, starRating[k].ariaChecked);
+        }
+        
+    }
+}
+
+for (var j = 0; j < starRating.length; j++) {
+    starRating[j].addEventListener = ('product', function () {
+        console.log("listener")
+        for (var k = 0; k < starRating.length; k++){
+            console.log(starRating[k].ariaChecked)
+            console.log(starRating[k].ariaLabel)
+            localStorage.setItem(starRating[k].ariaLabel, starRating[k].ariaChecked);
+        }
+    });
+}
+
+//File Upload (ignore)
 for (var j = 0; j < fileUpload.length; j++) {
     fileUpload[j].addEventListener = ('product', function () {
 
     });
 }
 
-for (var i = 0; i < input.length; i++) {
-    input[i].addEventListener('input', function () {
-        console.log("1111")
-        console.log(this.value);
-        localStorage.setItem(this.id, this.value);
-    });
-}
-
+//Selects
 for (var j = 0; j < selects.length; j++) {
     selects[j].onclick = function () {
         console.log("click")
@@ -265,7 +290,6 @@ for (var j = 0; j < selects.length; j++) {
     }
 }
 
-
 for (var i = 0; i < selects.length; i++) {
     selects[i].addEventListener('select', function () {
         console.log("selects")
@@ -274,40 +298,38 @@ for (var i = 0; i < selects.length; i++) {
     });
 }
 
-/*
-
-for (var j = 0; j < datePreviousMonthButton.length; j++) {
-    datePreviousMonthButton[j].onclick = function () {
-        console.log("calendar1")
-        getSetDate()
+//Submit Button, clear local storage
+for (var j = 0; j < submitButton.length; j++) {
+    submitButton[j].onclick = function () {
+        localStorage.clear()
     }
 }
 
-for (var j = 0; j < dateNextMonthButton.length; j++) {
-    dateNextMonthButton[j].onclick = function () {
-        console.log("calendar2")
-        getSetDate()
-    }
+//Inputs
+for (var i = 0; i < input.length; i++) {
+    input[i].addEventListener('input', function () {
+        console.log("1111")
+        console.log(this.value);
+        localStorage.setItem(this.id, this.value);
+    });
 }
 
-for (var j = 0; j < datePreviousYearButton.length; j++) {
-    datePreviousYearButton[j].onclick = function () {
-        console.log("calendar3")
-        getSetDate()
-    }
-}
-
-for (var j = 0; j < dateNextYearButton.length; j++) {
-    dateNextYearButton[j].setAttribute('onclick', 'getSetDate();');
-} */
-
-
+//Get date inputs and save to local storage 
 function getSetDate() {
     console.log("getSetDate")
     for (var j = 0; j < date.length; j++) {
         console.log(date)
         localStorage.setItem(date[j].id, date[j].value);
         console.log(date[j].value)
+    }
+}
+
+//Get date inputs and save to local storage 
+function getSetDateRequired() {
+    console.log("getSetDate")
+    for (var j = 0; j < dateRequired.length; j++) {
+        localStorage.setItem(dateRequired[j].id, dateRequired[j].value);
+        console.log(dateRequired[j].value)
     }
 }
 
@@ -325,11 +347,10 @@ document.onkeydown = function () {
     console.log(valuestext)
 }*/
 
-const count = 0;
-
+//Get inputs for every keydown
 document.onkeydown = function () {
     console.log("key down")
-    //input.addEventListener('change', updateValue);
+
     for (var i = 0; i < input.length; i++) {
         input[i].addEventListener('input', function () {
             console.log("hjjj")
@@ -340,26 +361,21 @@ document.onkeydown = function () {
 
     for (var i = 0; i < textareas.length; i++) {
         textareas[i].addEventListener('input', function () {
-            //console.log("text")
-            //console.log(this.value);
             localStorage.setItem(this.id, this.value);
         });
     }
 
     for (var j = 0; j < phone.length; j++) {
         localStorage.setItem(phone[j].id, phone[j].value);
-        //console.log(phone[j].value)
     }
 
     for (var j = 0; j < date.length; j++) {
         localStorage.setItem(date[j].id, date[j].value);
-        //console.log(date[j].value)
     }
 
-
-    //const x = document.getElementsByClassName("form-textbox validate[limitDate, validateLiteDate]");
-    //date[0].addEventListener("click", click_handler1, false);
-
+    for (var j = 0; j < dateRequired.length; j++) {
+        localStorage.setItem(dateRequired[j].id, dateRequired[j].value);
+    }
 };
 
 
